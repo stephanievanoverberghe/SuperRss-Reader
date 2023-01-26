6e2d1092dfdb7e6084860f5cf96a5b528ec37089
Perrine <perrine.morsch@gmail.com>
2023-01-26 14:07:23 +0100

    rectif

--- views/templates/header.php
+++ views/templates/header.php
@@ -59,13 +59,13 @@ if (isset($_COOKIE['subjectsUrls'])) {
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav d-flex align-self-center justify-content-around mb-2 mb-lg-0 mx-auto">
                         <li class="nav-item px-lg-4 px-xxl-5">
-                            <a class="nav-link mx-2 mx-xxl-5" href='../../controllers/pages-controller.php?sujet=1'><?= $subjectOne ?? 'Sujet 1' ?></a>
+                            <a class="nav-link mx-2 mx-xxl-5" href='../../controllers/pages-controller.php?sujet=1'><?= $subjectOne ?? '' ?></a>
                         </li>
                         <li class="nav-item px-lg-4 px-xxl-5">
-                            <a class="nav-link mx-2 mx-xxl-5" href="../../controllers/pages-controller.php?sujet=2"><?= $subjectTwo ?? 'Sujet 2' ?></a>
+                            <a class="nav-link mx-2 mx-xxl-5" href="../../controllers/pages-controller.php?sujet=2"><?= $subjectTwo ?? '' ?></a>
                         </li>
                         <li class="nav-item px-lg-4 px-xxl-5">
-                            <a class="nav-link mx-2 mx-xxl-5" href="../../controllers/pages-controller.php?sujet=3"><?= $subjectThree ?? 'Sujet 3' ?></a>
+                            <a class="nav-link mx-2 mx-xxl-5" href="../../controllers/pages-controller.php?sujet=3"><?= $subjectThree ?? '' ?></a>
                         </li>
                     </ul>
                     <ul class="navbar-nav d-flex align-items-lg-center mb-2 mb-lg-0">
