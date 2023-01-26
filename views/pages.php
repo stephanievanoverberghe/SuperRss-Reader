ab1cdc2c755bd60cff5f1a51ac231c8585d886a5
Perrine <perrine.morsch@gmail.com>
2023-01-26 14:06:06 +0100

    rectif design

--- views/pages.php
+++ views/pages.php
@@ -9,23 +9,25 @@
         </div>
         <div class="row justify-content-around align-items-center mt-5">
 
-            <div class="col-12 col-lg-3 d-flex align-items-center px-5 py-5">
-                <?php foreach ($urlItems as $article) {
-                    $namespaces = $article->getNamespaces(true);
-                    $namespaceDc = $article->children($namespaces['dc']);
-                    $date = date("j / m / Y ", (strtotime($namespaceDc->date)));
-                ?>
-                    <div class="card mt-5">
+
+            <?php foreach ($urlItems as $article) {
+                $namespaces = $article->getNamespaces(true);
+                $namespaceDc = $article->children($namespaces['dc']);
+                $date = date("j / m / Y ", (strtotime($namespaceDc->date)));
+            ?>
+                <div class="col-12 col-md-6 col-lg-4 d-flex align-items-center px-5 py-5">
+                    <div class="card mt-5 shadow-lg">
                         <img src="<?= $article->enclosure['url'] ?? '' ?>" class="card-img-top" alt="...">
                         <small class="text-center"><?= $date ?? '' ?></small>
-                        <div class="card-body text-center">
+                        <div class="card-body text-center d-flex flex-column justify-content-around">
                             <h5 class="card-title"><?= $article->title ?? '' ?></h5>
                             <p class="card-text"><?= $article->description ?? '' ?></p>
-                            <a href="<?= $article ?? '' ?>" class="btn px-5 py-2">Lire l'article</a>
+                            <a href="<?= $article->link ?? '' ?>" class="btn px-5 py-3 fs-5">Lire l'article</a>
                         </div>
                     </div>
-                <?php } ?>
-            </div>
+                </div>
+            <?php } ?>
+
         </div>
     </div>
 </main>
