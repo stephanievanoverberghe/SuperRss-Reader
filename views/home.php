e81f57512d6fd69f9c16e759471c1982a2c2e3d4
Perrine <perrine.morsch@gmail.com>
2023-01-26 12:28:00 +0100

    rectif du design

--- views/home.php
+++ views/home.php
@@ -12,9 +12,9 @@
         <div class="row justify-content-around align-items-center mt-5">
 
             <!-- SUJET 1 -->
-            <div class="col-12 col-lg-3 d-flex flex-column align-items-center columnSubject py-5">
+            <div class="col-12 col-md-5 col-lg-3 d-flex flex-column align-items-center columnSubject py-5 mt-5 mt-lg-0">
                 <div>
-                    <h2 class="text-center"><?= $subjectOne ?? '' ?></h2>
+                    <h2 class="text-center mt-5 mt-lg-0"><?= $subjectOne ?? '' ?></h2>
                 </div>
                 <?php foreach ($url1Items as $article) {
                     $namespaces = $article->getNamespaces(true);
@@ -24,17 +24,17 @@
                     <div class="card mt-5">
                         <img src="<?= $article->enclosure['url'] ?? '' ?>" class="card-img-top" alt="Article Image">
                         <small class="text-center">Publié le <?= $date ?></small>
-                        <div class="card-body text-center">
+                        <div class="card-body text-center d-flex flex-column justify-content-around">
                             <h5 class="card-title"><?= $article->title ?? '' ?></h5>
                             <p class="card-text"><?= $article->description ?? '' ?></p>
-                            <a href="<?= $article->link ?? '' ?>" target="_blank" class="btn px-5 py-2">Lire l'article</a>
+                            <a href="<?= $article->link ?? '' ?>" target="_blank" class="btn px-5 py-3 fs-5">Lire l'article</a>
                         </div>
                     </div>
                 <?php } ?>
             </div>
 
             <!-- SUJET 2 -->
-            <div class="col-12 col-lg-3 d-flex flex-column align-items-center columnSubject py-5 mt-5 mt-lg-0">
+            <div class="col-12 col-md-5 col-lg-3 d-flex flex-column align-items-center columnSubject py-5 mt-5 mt-lg-0">
                 <h2 class="text-center mt-5 mt-lg-0"><?= $subjectTwo ?? '' ?></h2>
                 <?php foreach ($url2Items as $article) {
                     $namespaces = $article->getNamespaces(true);
@@ -43,16 +43,16 @@
                     <div class="card mt-5">
                         <img src="<?= $article->enclosure['url'] ?? '' ?>" class="card-img-top" alt="Article Image">
                         <small class="text-center">Publié le <?= $date ?></small>
-                        <div class="card-body text-center">
+                        <div class="card-body text-center d-flex flex-column justify-content-around">
                             <h5 class="card-title"><?= $article->title ?? '' ?></h5>
                             <p class="card-text"><?= $article->description ?? '' ?></p>
-                            <a href="<?= $article->link ?? '' ?>" target="_blank" class="btn px-5 py-2">Lire l'article</a>
+                            <a href="<?= $article->link ?? '' ?>" target="_blank" class="btn px-5 py-3 fs-5">Lire l'article</a>
                         </div>
                     </div>
                 <?php } ?>
             </div>
             <!-- SUJET 3 -->
-            <div class="col-12 col-lg-3 d-flex flex-column align-items-center columnSubject py-5 mt-5 mt-lg-0">
+            <div class="col-12 col-md-5 col-lg-3 d-flex flex-column align-items-center columnSubject py-5 mt-5 mt-lg-0">
                 <h2 class="text-center mt-5 mt-lg-0"><?= $subjectThree ?? '' ?></h2>
                 <?php foreach ($url3Items as $article) {
                     $namespaces = $article->getNamespaces(true);
@@ -61,10 +61,10 @@
                     <div class="card mt-5">
                         <img src="<?= $article->enclosure['url'] ?? '' ?>" class="card-img-top" alt="Article Image">
                         <small class="text-center">Publié le <?= $date ?></small>
-                        <div class="card-body text-center">
+                        <div class="card-body text-center d-flex flex-column justify-content-around">
                             <h5 class="card-title"><?= $article->title ?? '' ?></h5>
                             <p class="card-text"><?= $article->description ?? '' ?></p>
-                            <a href="<?= $article->link ?? '' ?>" target="_blank" class="btn px-5 py-2">Lire l'article</a>
+                            <a href="<?= $article->link ?? '' ?>" target="_blank" class="btn px-5 py-3 fs-5">Lire l'article</a>
                         </div>
                     </div>
                 <?php } ?>
